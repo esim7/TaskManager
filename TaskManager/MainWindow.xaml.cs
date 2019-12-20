d59f5b9ce2dcf4fc121ba649335f6a123ff986d3
esim7 <makishev@yahoo.com>
2019-12-20 21:17:29 +0600

    Загрузил таск менеджер

---
+++ TaskManager/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TaskManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var Processes = Process.GetProcesses();
+            dataGrid.ItemsSource = Processes;
+        }
+
+        private void CloseProcessButton(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var thisProcess = (Process)dataGrid.SelectedItem;
+                thisProcess.Kill();
+                dataGrid.ItemsSource = null;
+                var Processes = Process.GetProcesses();
+                dataGrid.ItemsSource = Processes;
+            }
+            catch
+            {
+                MessageBox.Show("Отказано в доступе");
+            }         
+        }
+    }
+}
